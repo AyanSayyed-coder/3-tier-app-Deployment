d7a029f0a38b97a92281700e59bd180a69b8c91a
AyanSayyed-coder <ayansayyed30@gmail.com>
2026-02-28 12:52:04 +0530

    Update vpc.tf

--- terra-config/vpc.tf
+++ terra-config/vpc.tf
@@ -1,12 +1,12 @@
-# Get Default VPC
-data "aws_vpc" "default" {
-  default = true
-}
-
-# Get default Subnet ids
+# Get only supported subnets from default VPC
 data "aws_subnets" "public" {
   filter {
     name   = "vpc-id"
     values = [data.aws_vpc.default.id]
+  }
+
+  filter {
+    name   = "availability-zone"
+    values = ["us-east-1a", "us-east-1b"]
   }
 }
